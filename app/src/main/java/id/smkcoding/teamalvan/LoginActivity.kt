2f9f27458bd3947467a9ef28f83d5c396d4f3d4f
Rifky Galuh <56381009+rfqgal@users.noreply.github.com>
2020-06-30 17:55:34 +0700

    SMKCoding Team Alvan

--- app/src/main/java/id/smkcoding/teamalvan/LoginActivity.kt
+++ app/src/main/java/id/smkcoding/teamalvan/LoginActivity.kt
@@ -2,11 +2,14 @@ package id.smkcoding.teamalvan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        progress.visibility = View.GONE
     }
 }
