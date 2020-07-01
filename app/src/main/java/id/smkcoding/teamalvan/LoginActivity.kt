98b84c8e3ab2a0341120bbf37cb0f82fda33d416
danielriky <danielriky30@gmail.com>
2020-07-01 19:59:37 +0700

    facebook auth

--- app/src/main/java/id/smkcoding/teamalvan/LoginActivity.kt
+++ app/src/main/java/id/smkcoding/teamalvan/LoginActivity.kt
@@ -2,27 +2,42 @@ package id.smkcoding.teamalvan
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     private var auth: FirebaseAuth? = null
+    var callbackManager: CallbackManager? = null
+
     private val RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        auth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
         progress.visibility = View.GONE
-        btn_google_login.setOnClickListener(this)
-        auth = FirebaseAuth.getInstance() //Mendapakan Instance Firebase Auth
 
+        auth = FirebaseAuth.getInstance() //Mendapakan Instance Firebase Auth
+        //printkeyhash()
         //Cek apakah sudah login atau belum
         if (auth!!.currentUser == null) {
         } else {
@@ -31,7 +46,46 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
             startActivity(intent)
             finish()
         }
+        btn_google_login.setOnClickListener(this)
+        btn_facebook_login.setOnClickListener{
+            signIn()
+        }
     }
+
+    private fun signIn() {
+        btn_facebook_login.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                handleFacebookAccessToken(result!!.accessToken)
+            }
+
+            override fun onCancel() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(error: FacebookException?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+    private fun handleFacebookAccessToken(accessToken: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(accessToken.token)
+        auth!!.signInWithCredential(credential)
+            .addOnFailureListener { e ->
+                Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                Log.e("ERROR_EDMT",e.message)
+            }
+            .addOnSuccessListener { result ->
+
+                Toast.makeText(this, "Selamat Datang", Toast.LENGTH_LONG)
+                    .show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+    }
+
+
 
     override fun onActivityResult(
         requestCode: Int,
@@ -52,6 +106,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 Toast.makeText(this, "Login Dibatalkan", Toast.LENGTH_SHORT).show()
             }
         }
+        callbackManager!!.onActivityResult(requestCode,resultCode,data)
     }
 
     override fun onClick(v: View?) {
@@ -65,5 +120,22 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
             RC_SIGN_IN)
         progress.visibility = View.VISIBLE
     }
+
+    private fun printkeyhash() {
+        try {
+            val info = packageManager.getPackageInfo("com.example.chalenge2", PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures)
+            {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KEYHASH", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+        } catch (e: NoSuchAlgorithmException) {
+        }
+    }
+    
 }
+
+
 
