32c82fdb88002106242c16eed7d99906a8b7994c
Rifky Galuh <56381009+rfqgal@users.noreply.github.com>
2020-07-08 15:41:24 +0700

    Update Form Pengajuan Dokter

--- app/src/main/java/id/smkcoding/teamalvan/FormDokterActivity.kt
+++ app/src/main/java/id/smkcoding/teamalvan/FormDokterActivity.kt
@@ -4,22 +4,24 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import id.smkcoding.teamalvan.model.DoctorModel
 import kotlinx.android.synthetic.main.activity_form_dokter.*
-import java.util.jar.Manifest
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class FormDokterActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -27,22 +29,29 @@ class FormDokterActivity : AppCompatActivity(), View.OnClickListener {
     private val IDENTITY_PICK_CODE: Int = 101
     private val STR_PICK_CODE: Int = 102
 
-    lateinit var ref: DatabaseReference
+    lateinit var databaseReference: DatabaseReference
+    lateinit var storageReference: StorageReference
     lateinit var auth: FirebaseAuth
-    private lateinit var photoIdentity: Uri
-    private lateinit var photoStr: Uri
+    private lateinit var photoIdentity: EditText
+    private lateinit var photoStr: EditText
+
+    var uploadIdentitySuccess: Boolean = false
+    var uploadStrSuccess: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_dokter)
         title = "Form Pengajuan Akun Dokter"
 
-        ref = FirebaseDatabase.getInstance().reference
+        databaseReference = FirebaseDatabase.getInstance().reference
+        storageReference = FirebaseStorage.getInstance().reference
         auth = FirebaseAuth.getInstance()
 
         setSpinner(R.array.gender, sp_form_gender_dokter)
         setSpinner(R.array.jenis_dokter, sp_form_jenis_dokter)
 
+        btn_foto_identitas.setOnClickListener(this)
+        btn_foto_bukti_dokter.setOnClickListener(this)
         btn_ajukan.setOnClickListener(this)
     }
 
@@ -59,9 +68,9 @@ class FormDokterActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.btn_ajukan -> pengajuanForm()
             R.id.btn_foto_identitas -> getPhoto(IDENTITY_PICK_CODE)
             R.id.btn_foto_bukti_dokter -> getPhoto(STR_PICK_CODE)
+            R.id.btn_ajukan -> uploadImagesStatus()
         }
     }
 
@@ -70,13 +79,20 @@ class FormDokterActivity : AppCompatActivity(), View.OnClickListener {
         val getGender: String = sp_form_gender_dokter.selectedItem.toString()
         val getDoctorCat: String = sp_form_jenis_dokter.selectedItem.toString()
         val getDoctorID: String = edt_id_dokter?.text.toString()
-        val getPhotoIdentity: Uri = photoIdentity
-        val getPhotoStr: Uri = photoStr
+        val getPhotoIdentity: String = photoIdentity.text.toString()
+        val getPhotoStr: String = photoStr.text.toString()
         val getUserID: String = auth?.currentUser?.uid.toString()
 
         if (validateForm(getName, getDoctorID, getPhotoIdentity, getPhotoStr)) {
-            val doctor = DoctorModel(getName, getGender, getDoctorCat, getDoctorID, getPhotoIdentity, getPhotoStr)
-            ref.child(getUserID)
+            val doctor = DoctorModel(
+                getName,
+                getGender,
+                getDoctorCat,
+                getDoctorID,
+                getPhotoIdentity,
+                getPhotoStr
+            )
+            databaseReference.child(getUserID)
                 .child("tb_calon_dokter")
                 .push()
                 .setValue(doctor)
@@ -126,15 +142,82 @@ class FormDokterActivity : AppCompatActivity(), View.OnClickListener {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
-                IDENTITY_PICK_CODE -> photoIdentity = data?.data!!
-                STR_PICK_CODE -> photoStr = data?.data!!
+                IDENTITY_PICK_CODE -> {
+                    img_identity.visibility = View.VISIBLE
+                    val uri: Uri? = data!!.data
+                    img_identity.setImageURI(uri)
+                }
+                STR_PICK_CODE -> {
+                    img_str.visibility = View.VISIBLE
+                    val uri: Uri? = data!!.data
+                    img_str.setImageURI(uri)
+                }
             }
         } else {
             showToast(this, "Proses telah dibatalkan")
         }
     }
 
-    private fun validateForm(name: String, id: String, photoIdentity: Uri, photoStr: Uri): Boolean {
+    private fun uploadIdentity() {
+        //Mendapatkan data dari ImageView sebagai Bytes
+        val identity = (img_identity!!.drawable as BitmapDrawable).bitmap
+        val stream = ByteArrayOutputStream()
+
+        //Mengkompress bitmap menjadi JPG dengan kualitas gambar 100%
+        identity.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        val bytes: ByteArray = stream.toByteArray()
+
+        //Lokasi lengkap dimana gambar akan disimpan
+
+        val namaFile = UUID.randomUUID().toString() + ".jpg"
+        val pathImage = storageReference.child("gambar/ $namaFile")
+        val pathInsert = pathImage.downloadUrl.toString()
+        photoIdentity.setText(pathInsert);
+
+        val uploadTask = storageReference.child(pathImage.toString()).putBytes(bytes)
+        uploadTask
+            .addOnSuccessListener {
+                Toast.makeText(this, "Uploading Successful", Toast.LENGTH_SHORT).show()
+                uploadIdentitySuccess = true
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Uploading Failed", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun uploadStr() {
+        //Mendapatkan data dari ImageView sebagai Bytes
+        val str = (img_str!!.drawable as BitmapDrawable).bitmap
+        val stream = ByteArrayOutputStream()
+
+        //Mengkompress bitmap menjadi JPG dengan kualitas gambar 100%
+        str.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        val bytes: ByteArray = stream.toByteArray()
+
+        //Lokasi lengkap dimana gambar akan disimpan
+
+        val namaFile = UUID.randomUUID().toString() + ".jpg"
+        val pathImage = storageReference.child("gambar/ $namaFile")
+        val pathInsert = pathImage.downloadUrl.toString()
+        photoStr.setText(pathInsert);
+
+        val uploadTask = storageReference.child(pathImage.toString()).putBytes(bytes)
+        uploadTask
+            .addOnSuccessListener {
+                Toast.makeText(this, "Uploading Successful", Toast.LENGTH_SHORT).show()
+                uploadStrSuccess = true
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Uploading Failed", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun uploadImagesStatus() {
+        uploadIdentity().also { uploadStr() }
+        if (uploadIdentitySuccess && uploadStrSuccess) pengajuanForm()
+    }
+
+    private fun validateForm(name: String, id: String, photoIdentity: String, photoStr: String): Boolean {
         if (TextUtils.isEmpty(name)) {
             showToast(applicationContext, "Enter category!")
             return false
@@ -143,11 +226,11 @@ class FormDokterActivity : AppCompatActivity(), View.OnClickListener {
             showToast(applicationContext, "Enter field!")
             return false
         }
-        if (TextUtils.isEmpty(photoIdentity.toString())) {
+        if (TextUtils.isEmpty(photoIdentity)) {
             showToast(applicationContext, "Enter field!")
             return false
         }
-        if (TextUtils.isEmpty(photoStr.toString())) {
+        if (TextUtils.isEmpty(photoStr)) {
             showToast(applicationContext, "Enter field!")
             return false
         }
--- app/src/main/java/id/smkcoding/teamalvan/model/DoctorModel.kt
+++ app/src/main/java/id/smkcoding/teamalvan/model/DoctorModel.kt
@@ -1,12 +1,10 @@
 package id.smkcoding.teamalvan.model
 
-import android.net.Uri
-
 data class DoctorModel(
     var name: String,
     var gender: String,
     var category: String,
     var doctorID: String,
-    var identity: Uri,
-    var str: Uri
+    var identity: String,
+    var str: String
 )
