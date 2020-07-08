d9c2031f2f4a1c62acdc873da479288eda3a6c2a
Rifky Galuh <56381009+rfqgal@users.noreply.github.com>
2020-07-08 18:59:52 +0700

    Update Form Pengajuan Dokter

---
+++ app/src/main/java/id/smkcoding/teamalvan/ListPengajuanDokterActivity.kt
@@ -0,0 +1,12 @@
+package id.smkcoding.teamalvan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListPengajuanDokterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_pengajuan_dokter)
+    }
+}
